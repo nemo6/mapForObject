9ce38ea41a9382609bfad5040e82990e95a02e3e
nemo6 <miguel.maudet.contact@gmail.com>
2022-10-10 11:18:23 +0200

    Update 5 parent.js

--- 5 parent.js
+++ 5 parent.js
@@ -1,12 +1,12 @@
 function mapForObject(x,callback,arr=[]){
 
-for ( let y of Object.keys(x) )
-if ( typeof x[y] != 'object' )
-( x => x && arr.push(x) )(callback(x[y],y,x)) // je rajoute le x en plus, c'est l'objet qui contient la valeur ( ou la clé ) que je cherche
-else
-mapForObject(x[y],callback,arr)
+	for ( let y of Object.keys(x) )
+	if ( typeof x[y] != 'object' )
+	( x => x && arr.push(x) )(callback(x[y],y,x)) // je rajoute le x en plus, c'est l'objet qui contient la valeur ( ou la clé ) que je cherche
+	else
+	mapForObject(x[y],callback,arr)
 
-return arr
+	return arr
 
 }
 
